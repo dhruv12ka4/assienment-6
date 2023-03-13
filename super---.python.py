f77221211eae2e83932dd6d81b58fa3c39b4eb82
dhruv12ka4 <125910898+dhruv12ka4@users.noreply.github.com>
2023-03-13 21:27:09 +0530

    Add files via upload

---
+++ super---.python.py
@@ -0,0 +1,41 @@
+def add(a,b):
+    return a+b
+
+plus = lambda a,b:a+b
+print(plus(10,25))
+
+
+
+
+
+
+
+
+
+
+##list = [1,2,3,4,5,6,7,8,9]
+##even = 0
+##odd = 0
+##for i in list:
+##    if(i%2==0):
+##        even=even+1
+##    else:
+##        odd=odd+1
+##
+##print("Even number in list",even)
+##print("Odd number in list",odd)
+##
+##
+## 
+##
+##a = input("Enter a string:")
+##for word in a.split():
+##    print(word[::-1],end=" ")
+
+
+
+
+
+
+
+   
